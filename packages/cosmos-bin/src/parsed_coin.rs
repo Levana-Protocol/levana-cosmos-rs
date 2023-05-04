c9326e740f55e021df3d9ed6bed56efb782ed967
lvn-ryu <ryu@levana.exchange>
2023-05-04 15:41:06 +0300

    token parse

--- packages/cosmos-bin/src/parsed_coin.rs
+++ packages/cosmos-bin/src/parsed_coin.rs
@@ -22,7 +22,7 @@ impl From<ParsedCoin> for Coin {
 
 // Regex to capture the amount and denom of a coin.
 // ^ - start of string
-// ([0-9]+) - first capture group, the amount: one or more digits
+// (\d+) - first capture group, the amount: one or more digits
 // ([a-zA-z0-9/]+) - second capture group, the denom: any character in the range a-z, A-Z, 0-9, or /
 // $ - end of string
 static COIN_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"^(\d+)([a-zA-z0-9/]+)$").unwrap());
