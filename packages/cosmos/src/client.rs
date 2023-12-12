96b5ef417e6bbd6ccf95f7f2a6524d887f5291f8
lvn-hasky-dragon <hasky-dragon@levana.exchange>
2023-12-12 14:50:23 +0530

    Logging on successful account sequence mismatch retry
    Right now, I'm unable to understand if the new patches is actually
    improving thing.

--- packages/cosmos/src/client.rs
+++ packages/cosmos/src/client.rs
@@ -1145,7 +1145,11 @@ impl TxBuilder {
                     let sequence = cosmos.get_expected_sequence(status.message());
                     match sequence {
                         Some(new_sequence_no) => {
-                            return self.simulate_inner(cosmos, &[new_sequence_no]).await
+                            let result = self.simulate_inner(cosmos, &[new_sequence_no]).await;
+                            if result.is_ok() {
+                                tracing::info!("Retry of broadcast simulation failure succeeded with new sequence number of {new_sequence_no}");
+                            }
+                            return result;
                         }
                         None => return result,
                     }
